825f27da94b6e929a697e074bf5c8634373bd891
yuki <y.yamada6426@gmail.com>
2017-06-02 18:35:54 +0900

    フォーマットを調整

--- bms_to_json.py
+++ bms_to_json.py
@@ -15,15 +15,15 @@ def read_header(bms, key, is_int):
     if key == "WAV":
         wav = []
         while head != -1:
-            start = head+len(key)+3
+            start = head + len(key) + 3
             end = bms.find("\n", head)
             wav += bms[start:end] + ","
             search_key = "#{}".format(key)
-            head = bms.find(search_key, head+1)
+            head = bms.find(search_key, head + 1)
             if head == -1:
                 head = bms.find(search_key.upper())
         return wav
-    start = head+len(key)+1
+    start = head + len(key) + 1
     end = bms.find("\n", head)
     ret = bms[start:end]
     if is_int is True:
@@ -34,7 +34,7 @@ def read_header(bms, key, is_int):
 def slice_two(data, digit=10):
     num = []
     for i in range(0, len(data), 2):
-        num_text = data[i:i+2]
+        num_text = data[i:i + 2]
         if num_text.isdigit():
             num.append(int(num_text, digit))
     return num
@@ -46,23 +46,19 @@ def read_main(bms):
     main_data = []
     while head != -1:
         for i in range(11, 14):
-            head = bms.find("#", head+1)
+            head = bms.find("#", head + 1)
             if head == -1:
                 break
-            lane = int(bms[head+4:head+4+2])
+            lane = int(bms[head + 4:head + 4 + 2])
             if lane not in range(11, 14):
                 continue
-            if int(bms[head+1:head+1+3]) != measure or lane != i:
+            if int(bms[head + 1:head + 1 + 3]) != measure or lane != i:
                 head = head - 1
                 continue
             slice_start = bms.find(":", head) + 1
             slice_end = bms.find("\n", head)
             data = slice_two(bms[slice_start:slice_end])
-            main_object = {
-                "line": measure,
-                "channel": lane-11,
-                "data": data
-            }
+            main_object = {"line": measure, "channel": lane - 11, "data": data}
             main_data.append(main_object)
         measure += 1
     return main_data
@@ -74,11 +70,11 @@ def read_start(bms, initialBpm):
         exit(1)
     head = bms.find("MAIN DATA FIELD")
     while head != -1:
-        head = bms.find("#", head+1)
-        if int(bms[head+4:head+6]) != 1:
+        head = bms.find("#", head + 1)
+        if int(bms[head + 4:head + 6]) != 1:
             continue
-        line = int(bms[head+1:head+4])
-        slice_start = head+7
+        line = int(bms[head + 1:head + 4])
+        slice_start = head + 7
         slice_end = bms.find("\n", head)
         data = slice_two(bms[slice_start:slice_end], 10)
         # 1小節の秒数
@@ -92,30 +88,28 @@ def read_bpmchange(bms):
     bpmchange = []
     head = bms.find("MAIN DATA FIELD")
     while head != -1:
-        head = bms.find("#", head+1)
+        head = bms.find("#", head + 1)
         if head == -1:
             break
-        if int(bms[head+4:head+6]) == 3:
-            line = int(bms[head+1:head+4])
+        if int(bms[head + 4:head + 6]) == 3:
+            line = int(bms[head + 1:head + 4])
             index = bms.find(":", head)
-            slice_start = index+1
+            slice_start = index + 1
             slice_end = bms.find("\n", index)
             data = slice_two(bms[slice_start:slice_end], 16)
-            bpmchange.append({
-                "line": line,
-                "data": data
-            })
+            bpmchange.append({"line": line, "data": data})
     return bpmchange
+
 
 def calc_notes_weight(bms):
     head = bms.find("MAIN DATA FIELD")
-    #notesnum[i] 添え字が実際のbmsファイルのノーツ番号と対応
-    notesnum = [0,0,0,0,0,0,0,0]
+    # notesnum[i] 添え字が実際のbmsファイルのノーツ番号と対応
+    notesnum = [0, 0, 0, 0, 0, 0, 0, 0]
     while head != -1:
-        head = bms.find("#", head+1)
+        head = bms.find("#", head + 1)
         if head == -1:
             break
-        lane = int(bms[head+4:head+4+2])
+        lane = int(bms[head + 4:head + 4 + 2])
         if lane not in range(11, 14):
             continue
         slice_start = bms.find(":", head) + 1
@@ -128,43 +122,40 @@ def calc_notes_weight(bms):
 
     notessum = sum(notesnum)
     print("---notesrate-------------")
-    print("normal  : {0:>3} ({1:.1f}%)".format(notesnum[2],notesnum[2]*1.0/notessum*100))
-    print("red     : {0:>3} ({1:.1f}%)".format(notesnum[3],notesnum[3]*1.0/notessum*100))
-    print("long    : {0:>3} ({1:.1f}%)".format(notesnum[4],notesnum[4]*1.0/notessum*100))
-    print("slide   : {0:>3} ({1:.1f}%)".format(notesnum[5]+notesnum[6],(notesnum[5]+notesnum[6])*1.0/notessum*100))
-    print("special : {0:>3} ({1:.1f}%)".format(notesnum[7],notesnum[7]*1.0/notessum*100))
+    print("normal  : {0:>3} ({1:.1f}%)".format(notesnum[2], notesnum[2] * 1.0 /
+                                               notessum * 100))
+    print("red     : {0:>3} ({1:.1f}%)".format(notesnum[3], notesnum[3] * 1.0 /
+                                               notessum * 100))
+    print("long    : {0:>3} ({1:.1f}%)".format(notesnum[4], notesnum[4] * 1.0 /
+                                               notessum * 100))
+    print("slide   : {0:>3} ({1:.1f}%)".format(notesnum[5] + notesnum[6], (
+        notesnum[5] + notesnum[6]) * 1.0 / notessum * 100))
+    print("special : {0:>3} ({1:.1f}%)".format(notesnum[7], notesnum[7] * 1.0 /
+                                               notessum * 100))
+
     print("-------------------------")
 
     notes_weight = {
-        "normal" : 1,
-        "each"   : 2,
-        "long"   : 2,
-        "slide"  : 0.5,
+        "normal": 1,
+        "each": 2,
+        "long": 2,
+        "slide": 0.5,
         "special": 5
     }
     if (notesnum[5] + notesnum[6]) == 0:
         return notes_weight
-    
-    slide_weight = (notes_weight["normal"]*notesnum[2] + notes_weight["each"]*notesnum[3] * 0.6) / (notesnum[5]+notesnum[6])
+
+    slide_weight = (notes_weight["normal"] * notesnum[2] + notes_weight["each"]
+                    * notesnum[3] * 0.6) / (notesnum[5] + notesnum[6])
     if slide_weight < 0.5:
-        notes_weight[3] = round(slide_weight,3)
-        print "slide_weight is corrected"
-        
+        notes_weight[3] = round(slide_weight, 3)
+        print("slide_weight is corrected")
     return notes_weight
-    
-    
+
+
 def read_bms(filename):
-    header_string_list = [
-        "genre",
-        "title",
-        "artist",
-        "wav"
-    ]
-    header_integer_list = [
-        "bpm",
-        "playlevel",
-        "rank"
-    ]
+    header_string_list = ["genre", "title", "artist", "wav"]
+    header_integer_list = ["bpm", "playlevel", "rank"]
 
     header = {}
     main = []
@@ -180,7 +171,7 @@ def read_bms(filename):
     start = read_start(bms, header["bpm"])
     bpm = read_bpmchange(bms)
     notes_weight = calc_notes_weight(bms)
-    print notes_weight
+    print(notes_weight)
     json_object = {
         "header": header,
         "main": main,
